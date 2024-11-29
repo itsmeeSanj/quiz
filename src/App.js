c2af307cfb2274e99236a47a3eb4bb97e9999fc7
itsmeeSanj <itsmeesanj@gmail.com>
2024-11-28 19:04:51 -0500

    add:UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import DateCounter from "./DateCounter";
+// import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className='app'>
+      {/* <DateCounter /> */}
+      <Header />
+
+      <Main>
+        <div className='main'>
+          <p>1/15</p>
+          <p>Question?</p>
+        </div>
+      </Main>
     </div>
   );
 }
